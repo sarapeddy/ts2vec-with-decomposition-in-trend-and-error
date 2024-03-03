4849582142295e05bade39b5b43cc6289b379d18
sarapeddy <269362@studenti.unimore.it>
2024-03-03 18:35:37 +0100

    Creation of the custom dataset to have the moving_avg as features

---
+++ moving_avg_tensor_dataset.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from torch.tensor import Tensor
+from torch.utils.data import TensorDataset
+
+
+class MovingAvg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(MovingAvg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class TimeSeriesDatasetWithMovingAvg(TensorDataset):
+    
+    def __init__(self, original_dataset: Tensor, n_covariate_cols,  kernel_size=9):
+        self.n_covariate_cols = n_covariate_cols
+        self.moving_avg = MovingAvg(kernel_size, stride=1)
+        x_avg = self.moving_avg(original_dataset[:, :, self.n_covariate_cols:])
+        expanded_dataset = torch.cat([original_dataset, x_avg], dim=2)
+        super(TimeSeriesDatasetWithMovingAvg, self).__init__(expanded_dataset)
+
