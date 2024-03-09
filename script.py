7111075963370319aca835d6e6e27b969de9b33e
sarapeddy <269362@studenti.unimore.it>
2024-03-09 16:11:58 +0100

    script.py updated to handle the 3 type of modality for ts2vec

--- script.py
+++ script.py
@@ -11,9 +11,18 @@ import pandas as pd
 from configparser import ConfigParser
 from tasks.forecasting import eval_forecasting
 
+# RICORDATI DI MODIFICARE QUANDO HAI CREATO LA NUOVA CLASSE
+def create_model(type_of_train, dim, current_device, configuration):
+    if type_of_train == 'ts2vec-Dlinear':
+        return TS2Vec(input_dims=dim, device=current_device, **configuration)
+    else:
+        return TS2Vec(input_dims=dim, device=current_device, **configuration)
+
+
 # To configure the path to store the files and the dataset
 config = ConfigParser()
 config.read('config.ini')
+mode = config['EXECUTION TYPE'].get('mode')
 path = config['SETTINGS'].get('path')
 dataset = config['SETTINGS'].get('dataset')
 
@@ -34,7 +43,7 @@ torch.cuda.empty_cache()
 
 print("-------------- LOAD DATASET: PREPROCESSING ------------------------")
 
-data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols = datautils.load_forecast_csv(dataset)
+data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_time_cols = datautils.load_forecast_csv(dataset)
 # (Both train_data and test_data have a shape of n_instances x n_timestamps x n_features)
 
 print("Data after StandarScaler on n_coviariate_cols and original features")
@@ -44,7 +53,7 @@ print("train:", train_slice)
 print("valid: ", valid_slice)
 print("test:", test_slice)
 print("pred_lens: ", pred_lens)
-print("n_covariate_cols:", n_covariate_cols)
+print("n_time_cols:", n_time_cols)
 print('------------')
 print(scaler)
 
@@ -63,16 +72,17 @@ config = dict(
 run_dir = f'{path}/training/' + dataset + '__' + utils.name_with_datetime('forecast_multivar')
 os.makedirs(run_dir, exist_ok=True)
 
-print("\n------------------- TRAINING ENCODER -------------------")
+print("\n------------------- TRAINING ENCODER -------------------\n")
+
+input_dim = train_data.shape[-1]
+if mode == 'ts2vec-feature':
+    input_dim = train_data.shape[1] + train_data.shape[-1] - n_time_cols
 
 t = time.time()
 
 # Train a TS2Vec model
-model = TS2Vec(
-    input_dims=train_data.shape[-1] + train_data.shape[-1] - n_covariate_cols,
-    device=device,
-    **config
-)
+model = create_model(mode, input_dim, device, config)
+
 loss_log = model.fit(
     train_data,
     n_epochs=None,
@@ -85,11 +95,11 @@ model.save(f'{run_dir}/model.pkl')
 t = time.time() - t
 print(f"\nTraining time: {datetime.timedelta(seconds=t)}\n")
 
-print("\n----------------- EVAL FORECASTING -------------------")
+print("\n----------------- EVAL FORECASTING -------------------\n")
 
-out, eval_res = eval_forecasting(model, data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols)
+out, eval_res = eval_forecasting(model, data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_time_cols)
 
-print("\n----------------- FINAL RESULTS --------------------")
+print("\n----------------- FINAL RESULTS --------------------\n")
 
 utils.pkl_save(f'{run_dir}/out.pkl', out)
 utils.pkl_save(f'{run_dir}/eval_res.pkl', eval_res)
