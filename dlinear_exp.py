fee86a7100e9bb7c2dcccc9525972bb341209993
sarapeddy <269362@studenti.unimore.it>
2024-03-17 21:00:10 +0100

    Add the Dataset and the class to execute the model present in DLinear.py

---
+++ dlinear_exp.py
@@ -0,0 +1,302 @@
+import os
+import time
+
+import numpy as np
+from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+
+from data_provider_dlinear.data_factory import data_provider
+from utils_dlinear.tools_dlinear import EarlyStopping, adjust_learning_rate, visual, test_params_flop
+
+from models.DLinear import Model
+import torch
+from tasks.forecasting import cal_metrics
+from dlinear_dataset import DLinearDateset
+
+
+class DLinear:
+    def __init__(self, device, name_dataset='ETTm1', seq_len=96, pred_len=24, enc_in=7, individual=False, batch_size=8, lr=0.0001, label_len=48, use_amp=False, output_attention=False,
+                 features='MS', num_workers=10, checkpoints='./checkpoints', patience=3, train_epochs=10, test_flop=False, embed='timeF'):
+        self.device = device
+        self.name_dataset = name_dataset
+        self.lr = lr
+        self.batch_size = batch_size
+        self.label_len = label_len
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+        self.enc_in = enc_in
+        self.features = features
+        self.patience = patience
+        self.train_epochs = train_epochs
+        self.use_amp = use_amp
+        self.num_workers = num_workers
+        self.output_attention = output_attention
+        self.checkpoints = checkpoints
+        self.test_flop = test_flop
+        self.embed = embed
+        self.model = Model(seq_len, pred_len, enc_in, individual)
+
+    def _get_data(self, name_dataset, flag):
+        data_set, data_loader = data_provider(name_dataset, flag)
+        return data_set, data_loader
+
+    def _select_optimizer(self):
+        model_optim = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        return model_optim
+
+    def _select_criterion(self):
+        criterion = nn.MSELoss()
+        return criterion
+
+    def vali(self, vali_data, vali_loader, criterion):
+        total_loss = []
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(vali_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float()
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.use_amp:
+                    with torch.cuda.amp.autocast():
+                        outputs = self.model(batch_x)
+                else:
+                    outputs = self.model(batch_x)
+
+                f_dim = -1 if self.features == 'MS' else 0
+                outputs = outputs[:, -self.pred_len:, f_dim:]
+                batch_y = batch_y[:, -self.pred_len:, f_dim:].to(self.device)
+
+                pred = outputs.detach().cpu()
+                true = batch_y.detach().cpu()
+
+                loss = criterion(pred, true)
+
+                total_loss.append(loss)
+        total_loss = np.average(total_loss)
+        self.model.train()
+        return total_loss
+
+    def train(self, train_data, vali_data, test_data, scaler):
+
+        # SISTEMARE QUI IL DATASET E IL DATASET E IL DATALOADER
+        train_dataset = DLinearDateset(train_data, self.seq_len, self.pred_len, self.label_len)
+        vali_dataset = DLinearDateset(vali_data, self.seq_len, self.pred_len, self.label_len)
+        test_dataset = DLinearDateset(test_data, self.seq_len, self.pred_len, self.label_len)
+
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, drop_last=True)
+        vali_loader = DataLoader(vali_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, drop_last=True)
+        test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, drop_last=True)
+
+        # train_data, train_loader = self._get_data(self.name_dataset, 'train')
+        # vali_data, vali_loader = self._get_data(self.name_dataset, 'vali')
+        # test_data, test_loader = self._get_data(self.name_dataset, 'test')
+
+        path = os.path.join(self.checkpoints, 'DLinear')
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        time_now = time.time()
+        train_steps = len(train_loader)
+        early_stopping = EarlyStopping(patience=self.patience, verbose=True)
+
+        model_optim = self._select_optimizer()
+        criterion = self._select_criterion()
+
+        if self.use_amp:
+            scaler = torch.cuda.amp.GradScaler()
+
+        for epoch in range(self.train_epochs):
+            iter_count = 0
+            train_loss = []
+
+            self.model.train()
+            epoch_time = time.time()
+            for i, (batch_x, batch_y) in enumerate(train_loader):
+                iter_count += 1
+                model_optim.zero_grad()
+                batch_x = batch_x.float().to(self.device)
+
+                batch_y = batch_y.float().to(self.device)
+
+                # encoder - decoder
+                if self.use_amp:
+                    with torch.cuda.amp.autocast():
+                        outputs = self.model(batch_x)
+
+                        f_dim = -1 if self.features == 'MS' else 0
+                        outputs = outputs[:, -self.pred_len:, f_dim:]
+                        batch_y = batch_y[:, -self.pred_len:, f_dim:].to(self.device)
+                        loss = criterion(outputs, batch_y)
+                        train_loss.append(loss.item())
+                else:
+                    outputs = self.model(batch_x)
+
+                    # print(outputs.shape,batch_y.shape)
+                    f_dim = -1 if self.features == 'MS' else 0
+                    outputs = outputs[:, -self.pred_len:, f_dim:]
+                    batch_y = batch_y[:, -self.pred_len:, f_dim:].to(self.device)
+                    loss = criterion(outputs, batch_y)
+                    train_loss.append(loss.item())
+
+                if (i + 1) % 100 == 0:
+                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
+                    speed = (time.time() - time_now) / iter_count
+                    left_time = speed * ((self.train_epochs - epoch) * train_steps - i)
+                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    iter_count = 0
+                    time_now = time.time()
+
+                if self.use_amp:
+                    scaler.scale(loss).backward()
+                    scaler.step(model_optim)
+                    scaler.update()
+                else:
+                    loss.backward()
+                    model_optim.step()
+
+            print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+            train_loss = np.average(train_loss)
+            vali_loss = self.vali(vali_data, vali_loader, criterion)
+            test_loss = self.vali(test_data, test_loader, criterion)
+
+            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
+                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+            early_stopping(vali_loss, self.model, path)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+
+            adjust_learning_rate(model_optim, epoch + 1, self.lr)
+
+        best_model_path = path + '/' + 'checkpoint.pth'
+        self.model.load_state_dict(torch.load(best_model_path))
+
+        return self.model
+
+    def test(self, data, test_slice, results, test=0):
+        test_data = data[:, test_slice]
+        test_loader = DataLoader(test_data, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, drop_last=True)
+
+        if test:
+            print('loading model')
+            self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + f'Dlinear__{self.seq_len}__{self.pred_len}', 'checkpoint.pth')))
+
+        preds = []
+        trues = []
+        inputx = []
+        folder_path = './test_results/' + f'Dlinear__{self.seq_len}__{self.pred_len}' + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(test_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.use_amp:
+                    with torch.cuda.amp.autocast():
+                        outputs = self.model(batch_x)
+                else:
+                    outputs = self.model(batch_x)
+
+                f_dim = -1 if self.features == 'MS' else 0
+                # print(outputs.shape,batch_y.shape)
+                outputs = outputs[:, -self.pred_len:, f_dim:]
+                batch_y = batch_y[:, -self.pred_len:, f_dim:].to(self.device)
+                outputs = outputs.detach().cpu().numpy()
+                batch_y = batch_y.detach().cpu().numpy()
+
+                pred = outputs  # outputs.detach().cpu().numpy()  # .squeeze()
+                true = batch_y  # batch_y.detach().cpu().numpy()  # .squeeze()
+
+                preds.append(pred)
+                trues.append(true)
+                inputx.append(batch_x.detach().cpu().numpy())
+                if i % 20 == 0:
+                    input = batch_x.detach().cpu().numpy()
+                    gt = np.concatenate((input[0, :, -1], true[0, :, -1]), axis=0)
+                    pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
+                    visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
+
+        if self.test_flop:
+            test_params_flop((batch_x.shape[1], batch_x.shape[2]))
+            exit()
+        preds = np.array(preds)
+        trues = np.array(trues)
+        inputx = np.array(inputx)
+
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+        inputx = inputx.reshape(-1, inputx.shape[-2], inputx.shape[-1])
+
+        # result save
+        folder_path = './results/' + f'Dlinear__{self.seq_len}__{self.pred_len}' + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        results[self.pred_len] = {
+            'norm': cal_metrics(preds, trues),
+        }
+
+        # np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe,rse, corr]))
+        np.save(folder_path + 'pred.npy', preds)
+        # np.save(folder_path + 'true.npy', trues)
+        # np.save(folder_path + 'x.npy', inputx)
+        return results
+
+    def predict(self, data, pred_slice, load=False):
+        pred_data = data[:, pred_slice]
+        pred_loader = DataLoader(pred_data, batch_size=1, shuffle=False, num_workers=self.num_workers, drop_last=False)
+
+        if load:
+            path = os.path.join(self.checkpoints,  f'Dlinear__{self.seq_len}__{self.pred_len}')
+            best_model_path = path + '/' + 'checkpoint.pth'
+            self.model.load_state_dict(torch.load(best_model_path))
+
+        preds = []
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float()
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros([batch_y.shape[0], self.pred_len, batch_y.shape[2]]).float().to(
+                    batch_y.device)
+                dec_inp = torch.cat([batch_y[:, :self.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.use_amp:
+                    with torch.cuda.amp.autocast():
+                        outputs = self.model(batch_x)
+                else:
+                    outputs = self.model(batch_x)
+
+                pred = outputs.detach().cpu().numpy()  # .squeeze()
+                preds.append(pred)
+
+        preds = np.array(preds)
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+
+        # result save
+        folder_path = './results/' +  f'Dlinear__{self.seq_len}__{self.pred_len}' + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        np.save(folder_path + 'real_prediction.npy', preds)
+
+        return
