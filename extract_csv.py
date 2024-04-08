9610bae5fd92410a7862cbd4b1fbdea96de852ef
sarapeddy <269362@studenti.unimore.it>
2024-04-08 14:48:32 +0200

    Now the script can handle all the task and generate the respective result.csv file

--- extract_csv.py
+++ extract_csv.py
@@ -22,22 +22,36 @@ def extract_data_from_file(file_path):
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
-            data = data['ours']
-            match = re.search(r'([^/]+)__forecast_multivar_', file_path).group(1)
+
+            if 'forecasting' in args.directory:
+                data = data['ours']
+                match = re.search(r'([^/]+)__forecast_multivar_', file_path).group(1)
+            elif 'classification' in args.directory:
+                match = re.search(r'([^/]+)__classification_', file_path).group(1)
+            else:
+                raise ValueError(f"Unknown task type")
+
             splits = file_path.split('/')
-            model_name = splits[3]
+            model_name = splits[4] if 'forecasting' in args.directory else splits[3]
             extract_data = {model_name:{match: {}}}
             for key in data.keys():
-                extract_data[model_name][match][key] ={
-                    'MAE': round(data[key]['norm']['MAE'], 4),
-                    'MSE': round(data[key]['norm']['MSE'], 4)
-                }
+                if 'forecasting' in args.directory:
+                    extract_data[model_name][match][key] ={
+                        'MAE': round(data[key]['norm']['MAE'], 4),
+                        'MSE': round(data[key]['norm']['MSE'], 4)
+                    }
+                elif 'classification' in args.directory:
+                    extract_data[model_name][match] ={
+                        'acc': round(data[key], 4),
+                    }
+                else:
+                    raise ValueError(f"Unknown task type")
             return extract_data
     except Exception as e:
         print(f"Error in {file_path}: {e}")
         return None
 
-def extract_rows(data_list):
+def extract_rows_forecasting(data_list):
     rows = []
     for data in data_list:
         for model_name, predictions in data.items():
@@ -59,6 +73,25 @@ def extract_rows(data_list):
     df['Dataset'] = df['Dataset'].where(df['Dataset'] != df['Dataset'].shift(), " ")
     return df
 
+def extract_rows_classification(data_list):
+    rows = []
+    for data in data_list:
+        for model_name, datasets in data.items():
+            for dataset_name, metrics in datasets.items():
+                rows.append({
+                    'Model': model_name,
+                    'Dataset': dataset_name,
+                    'acc': metrics['acc']
+                })
+
+    df = pd.DataFrame(rows).sort_values(by=['Model', 'Dataset'])
+    df = df.pivot_table(index=['Dataset'], columns='Model', values=['acc'])
+    df.columns = [f'{model}' for metrics, model in df.columns]
+    df = df.reindex(sorted(df.columns), axis=1)
+    df.reset_index(inplace=True)
+    df['Dataset'] = df['Dataset'].where(df['Dataset'] != df['Dataset'].shift(), " ")
+    return df
+
 def main(directory, output_csv):
     """
     Extracts MAE e MSE from eval_res.json files and saves them in a CSV file.
@@ -66,13 +99,20 @@ def main(directory, output_csv):
     files = find_eval_res_files(directory)
     data_list = [extract_data_from_file(file) for file in files]
     data_list = [data for data in data_list if data is not None]  # Rimuovi eventuali None
-    df = extract_rows(data_list)
+
+    if 'forecasting' in args.directory:
+        df = extract_rows_forecasting(data_list)
+    elif 'classification' in args.directory:
+        df = extract_rows_classification(data_list)
+    else:
+        raise ValueError(f"Unknown task type")
+
     df.to_csv(f'{directory}/{output_csv}', index=False)
     print(f"Data saved in {output_csv}")
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Insertion of correct path to save the csv')
     parser.add_argument('--directory', type=str, help='Directory where the eval_res.json files are located')
-    args = parser.parse_args()
+    args = parser.parse_args(['--directory', 'forecasting/normal'])
     output_csv = "results.csv"  # Cambia il nome del file CSV se necessario
     main(f'./training/{args.directory}', output_csv)
