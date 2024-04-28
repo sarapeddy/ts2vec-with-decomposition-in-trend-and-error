c9762a8a064897a52dc7421316d2db60ab4e6c28
sarapeddy <269362@studenti.unimore.it>
2024-04-28 16:15:29 +0200

    add channel indipendence also in classification

--- script_classification.py
+++ script_classification.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 from configparser import ConfigParser
@@ -18,13 +19,30 @@ def create_model(type_of_train, dim, n_time_cols, current_device, configuration)
         return TS2VecDlinear(input_dims=dim, device=current_device, mode=type_of_train, n_time_cols=n_time_cols, **configuration)
     return TS2Vec(input_dims=dim, device=current_device, mode=type_of_train, **configuration)
 
-config = ConfigParser()
-config.read('config_classification.ini')
-mode = config['EXECUTION TYPE'].get('mode')
-path = config['SETTINGS'].get('path')
-loader = config['SETTINGS'].get('loader')
-dataset = config['SETTINGS'].get('dataset')
-batch_size = config['PARAMETERS'].getint('batch_size')
+# config = ConfigParser()
+# config.read('config_classification.ini')
+# mode = config['EXECUTION TYPE'].get('mode')
+# path = config['SETTINGS'].get('path')
+# loader = config['SETTINGS'].get('loader')
+# dataset = config['SETTINGS'].get('dataset')
+# batch_size = config['PARAMETERS'].getint('batch_size')
+# ci = config['PARAMETERS'].getboolean('ci')
+
+config = argparse.ArgumentParser()
+config.add_argument('--mode', type=str, default='feature')
+config.add_argument('--path', type=str, default='/dati/home/sara.pederzoli/project/ts2vec-main')
+config.add_argument('--loader', type=str, default='UCR')
+config.add_argument('--dataset', type=str, default='FordA')
+config.add_argument('--batch_size', type=int, default=8)
+config.add_argument('--ci', type=bool, default=True)
+
+args = config.parse_args()
+mode = args.mode
+path = args.path
+loader = args.loader
+dataset = args.dataset
+batch_size = args.batch_size
+ci = args.ci
 
 # set GPU
 device = utils.init_dl_program(0, seed=42, max_threads=8)
@@ -52,19 +70,30 @@ print("\n----------------- TRAINING ENCODER ------------------------\n")
 run_dir = f'{path}/training/classification/B_{batch_size}/{mode}/' + dataset + '__' + utils.name_with_datetime('classification')
 os.makedirs(run_dir, exist_ok=True)
 
-config = dict(
-    batch_size=batch_size,
-    lr=0.001,
-    output_dims=320,
-    max_train_length=3000,
-)
+if not ci:
+    config = dict(
+        batch_size=batch_size,
+        lr=0.001,
+        output_dims=320,
+        max_train_length=3000,
+    )
 
-input_dim = train_data.shape[-1]
-if mode == 'feature':
-    input_dim = train_data.shape[-1] * 2
+    input_dim = train_data.shape[-1]
+    if mode == 'feature':
+        input_dim = train_data.shape[-1] * 2
 
-# model = TS2Vec(input_dims=train_data.shape[-1], device=device, mode=mode, **config)
-model = create_model(mode, input_dim, 0, device, config)
+    model = create_model(mode, input_dim, 0, device, config)
+
+else:
+    config = dict(
+        batch_size=1,
+        lr=0.001,
+        output_dims=40,
+        max_train_length=3000,
+        ci=ci
+    )
+
+    model = create_model(mode, 1, 0, device, config)
 
 t = time.time()
 
@@ -85,7 +114,7 @@ print(f"\nTraining time: {datetime.timedelta(seconds=t)}\n")
 
 print("\n----------------- EVAL CLASSIFICATION -------------------\n")
 
-out, eval_res = eval_classification(model, train_data, train_labels, test_data, test_labels, eval_protocol='svm')
+out, eval_res = eval_classification(model, train_data, train_labels, test_data, test_labels, eval_protocol='svm', ci=ci)
 
 print("\n----------------- FINAL RESULTS --------------------\n")
 
