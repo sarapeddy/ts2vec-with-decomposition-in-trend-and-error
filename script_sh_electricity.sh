8c5800d557d8ea8653527841688cf2ed0cf46a91
sarapeddy <269362@studenti.unimore.it>
2024-06-17 10:02:32 +0200

    Script to run ts2vec on electricity dataset

---
+++ script_sh_electricity.sh
@@ -0,0 +1,5 @@
+
+# Il parametro path è da sostituire con il path alla cartella del progetto. Ad es. per me è così /dati/home/sara.pederzoli/project/ts2vec-main
+
+python3 script_forecasting.py --mode ts2vec-Dlinear-one-loss --dataset electricity --path 'insert_path_here'
+python3 script_forecasting.py --mode ts2vec --dataset electricity --path 'insert_path_here'
